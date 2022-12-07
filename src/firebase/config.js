0aed33ade385bc6053a95de1d69c03d1192b950a
Manali Darji <darjimanali93@gmail.com>
2022-12-07 17:40:36 -0500

    firebase deployment changes

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4GcLDWHsq-gO7A3xen2avb50o093wDqY",
+  authDomain: "online-lottery-23667.firebaseapp.com",
+  projectId: "online-lottery-23667",
+  storageBucket: "online-lottery-23667.appspot.com",
+  messagingSenderId: "607613242004",
+  appId: "1:607613242004:web:beae55903143328167c281"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
