f73bd3cdb2ed98d02e9ccd95a5a96e49ce96c9d4
Manali Darji <darjimanali93@gmail.com>
2022-12-11 21:48:15 -0500

    updated with dummy data

--- src/constants.js
+++ src/constants.js
@@ -2,31 +2,27 @@ export const PER_TICKET_COST = 50;
 export const TICKET_ID_DIGITS = 4;
 export const ALL_SELLERS = [
     {
-        seller_id: 'J',
-        seller_name: 'Jestin Joshi'
-    },
-    {
-        seller_id: 'X',
-        seller_name: 'Justin Xavier'
-    },
-    {
-        seller_id: 'P',
-        seller_name: 'Philcy Philip'
-    },
-    {
-        seller_id: 'G',
-        seller_name: 'Grejo Smith'
-    },
-    {
         seller_id: 'A',
-        seller_name: 'Janni P'
+        seller_name: 'A_seller_1'
     },
     {
-        seller_id: 'O',
-        seller_name: 'John Lopez'
+        seller_id: 'B',
+        seller_name: 'B_seller_2'
+    },
+    {
+        seller_id: 'C',
+        seller_name: 'C_seller_3'
+    },
+    {
+        seller_id: 'D',
+        seller_name: 'D_seller_4'
     },
     {
         seller_id: 'E',
-        seller_name: 'Jenny Maalpua'
+        seller_name: 'E_seller_5'
+    },
+    {
+        seller_id: 'F',
+        seller_name: 'F_seller_6'
     },
 ];
