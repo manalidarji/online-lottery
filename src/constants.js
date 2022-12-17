b97b68517fca17f0c25e0401ff15a696cd68a8c3
Jestin Joshi <jestinjoshi@gmail.com>
2022-12-18 00:27:44 +0530

    added anu

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,6 @@ export const PER_TICKET_COST = 50;
 export const TICKET_ID_DIGITS = 4;
 export const ALL_SELLERS = [
     {
-        seller_id: 5,
-        seller_name: 'Team Jestin'
-    },
-    {
         seller_id: 1,
         seller_name: 'Team Philcy'
     },
@@ -20,5 +16,13 @@ export const ALL_SELLERS = [
     {
         seller_id: 4,
         seller_name: 'Team Alen'
+    },
+    {
+        seller_id: 5,
+        seller_name: 'Team Jestin'
+    },
+    {
+        seller_id: 6,
+        seller_name: 'Team Anu'
     },
 ];
